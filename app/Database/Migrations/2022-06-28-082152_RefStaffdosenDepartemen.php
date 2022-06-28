d6b610bc5507ffc9c0c578a43de4da58fe652c80
gadingihsn <you@example.com>
2022-06-28 15:44:19 +0700

    Migration & Seeder Departemen, Jenispegawai, Unit

---
+++ app/Database/Migrations/2022-06-28-082152_RefStaffdosenDepartemen.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class RefStaffdosenDepartemen extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_departemen' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE,
+            ],
+            'nama_departemen' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ]
+        ]);
+        $this->forge->addKey('id_departemen', 'true');
+        $this->forge->createTable('ref_stafdosen_departemen');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('ref_stafdosen_departemen');
+    }
+}
