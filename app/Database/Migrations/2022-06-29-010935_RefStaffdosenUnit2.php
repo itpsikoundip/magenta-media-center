2828b954b4d8645467da7a64ca35503a936d88ac
gadingihsn <you@example.com>
2022-07-05 10:50:35 +0700

    Seed & Migration Staff Dosen

--- app/Database/Migrations/2022-06-29-010935_RefStaffdosenUnit2.php
+++ app/Database/Migrations/2022-06-29-010935_RefStaffdosenUnit2.php
@@ -4,28 +4,28 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class RefStaffdosenJenispegawai extends Migration
+class RefStaffdosenUnit2 extends Migration
 {
     public function up()
     {
         $this->forge->addField([
-            'id_jenispegawai' => [
+            'id_unit2' => [
                 'type' => 'INT',
                 'constraint' => 11,
                 'unsigned' => TRUE,
                 'auto_increment' => TRUE,
             ],
-            'nama_jenispegawai' => [
+            'nama_unit2' => [
                 'type' => 'VARCHAR',
                 'constraint' => '255',
             ]
         ]);
-        $this->forge->addKey('id_jenispegawai', 'true');
-        $this->forge->createTable('ref_stafdosen_jenispegawai');
+        $this->forge->addKey('id_unit2', 'true');
+        $this->forge->createTable('ref_stafdosen_unit2');
     }
 
     public function down()
     {
-        $this->forge->dropTable('ref_stafdosen_jenispegawai');
+        $this->forge->dropTable('ref_stafdosen_unit2');
     }
 }
