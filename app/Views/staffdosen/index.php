21500d51c6dade44507073bf6b0b9ca65949c4ba
gadingihsn <you@example.com>
2022-07-12 08:50:06 +0700

    update minor dsahboard staffdosen card helpdesk

--- app/Views/staffdosen/index.php
+++ app/Views/staffdosen/index.php
@@ -16,13 +16,19 @@
             <hr class="mb-2 mt-0">
             <div class="content-body">
                 <div class="row">
-                    <div class="col-xl-3 col-lg-6 col-12">
-                        <a class="card" href="<?= base_url('helpdeskstaffdosen/') ?>">
-                            <div class="card-content">
-                                <div class="card-body">
-                                    <div class="media d-flex">
-                                        <div class="align-self-center">
-                                            <i class="icon-bubbles info font-large-2 float-left"></i>
+                    <?php if (session()->get('helpdesk') == 1) { ?>
+                        <div class="col-xl-3 col-lg-6 col-12">
+                            <a class="card" href="<?= base_url('helpdeskstaffdosen/') ?>">
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <div class="media d-flex">
+                                            <div class="align-self-center">
+                                                <i class="icon-bubbles info font-large-2 float-left"></i>
+                                            </div>
+                                            <div class="media-body text-right">
+                                                <h3><u>Helpdesk</u></h3>
+                                                <span>Solusi Permasalahan</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
