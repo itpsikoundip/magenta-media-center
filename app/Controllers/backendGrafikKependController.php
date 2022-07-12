7ca697bebccd892d4525b622df5e81751307703d
asdwd5 <arielzzz90@hotmail.com>
2022-07-12 15:10:14 +0700

    grafik all done

--- app/Controllers/backendGrafikKependController.php
+++ app/Controllers/backendGrafikKependController.php
@@ -2,15 +2,75 @@
 
 namespace App\Controllers;
 
+
+use App\Models\surveyKependModel;
+use App\Models\hasilSurveyKependModel;
+use App\Models\singleKependModel;
+
 class backendGrafikKependController extends BaseController
 {
+
+    protected $surveyKependModel;
+    protected $hasilSurveyKependModel;
+    protected $singleKependModel;
+
+    public function __construct()
+    {
+        $this->surveyKependModel = new surveyKependModel();
+        $this->hasilSurveyKependModel = new hasilSurveyKependModel();
+        $this->singleKependModel = new singleKependModel();
+    }
+
     public function index()
     {
-        $data = [
-            'title' => 'Grafik Kepend',
-            'isi'    => 'admin/survey/grafikKepend'
-        ];
+        $displaySingleKepend = $this->singleKependModel->findAll();
 
-        return view('layouts/survey-wrapper', $data);
+        $array = array();
+
+        foreach ($displaySingleKepend as $row) {
+            array_push($array, $row["pertanyaan"]);
+        }
+
+        $arraySangatBaik = array();
+        $arrayBaik = array();
+        $arrayCukup = array();
+        $arrayBuruk = array();
+        $arraySangatBuruk = array();
+
+        for ($i = 0; $i < count($array); $i++) {
+            array_push($arraySangatBaik, $this->surveyKependModel->selectSangatBaik($array[$i])->getResult());
+            array_push($arrayBaik, $this->surveyKependModel->selectBaik($array[$i])->getResult());
+            array_push($arrayCukup, $this->surveyKependModel->selectCukup($array[$i])->getResult());
+            array_push($arrayBuruk, $this->surveyKependModel->selectBuruk($array[$i])->getResult());
+            array_push($arraySangatBuruk, $this->surveyKependModel->selectSangatBuruk($array[$i])->getResult());
+        }
+        //Sum Passing
+        if (count($array) == 0) {
+            $data = [
+                'title' => 'Grafik Kepend',
+                'arrayPertanyaan' => "Tidak ada pertanyaan survey kependidikan",
+                'sumSangatBaik' => 0,
+                'sumBaik' => 0,
+                'sumCukup' => 0,
+                'sumBuruk' => 0,
+                'sumSangatBuruk' => 0,
+                'isi'    => 'admin/survey/grafikKepend'
+            ];
+
+            return view('layouts/survey-wrapper', $data);
+        } else {
+            $data = [
+                'title' => 'Grafik Kepend',
+                'arrayPertanyaan' => $array,
+                'sumSangatBaik' => $arraySangatBaik,
+                'sumBaik' => $arrayBaik,
+                'sumCukup' => $arrayCukup,
+                'sumBuruk' => $arrayBuruk,
+                'sumSangatBuruk' => $arraySangatBuruk,
+                'isi'    => 'admin/survey/grafikKepend'
+            ];
+
+            return view('layouts/survey-wrapper', $data);
+        }
     }
 }
