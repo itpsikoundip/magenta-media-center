0c63fb28eb64f1df8a95eea7245fb8e7a51fa280
itpsikoundip <itpsikoundip@gmail.com>
2022-07-25 15:01:17 +0700

    delete faq di helpdesk staffdosen

--- app/Models/ModelHelpdeskStaffDosen.php
+++ app/Models/ModelHelpdeskStaffDosen.php
@@ -27,7 +27,7 @@ class ModelHelpdeskStaffDosen extends Model
     function getDetail($tiket_id){
         $tiket = $this->db->table('tiket')
                     ->where('id', $tiket_id)
-                    ->join('mahasiswa','mahasiswa.nim =  tiket.mahasiswa_id')
+                    ->join('mahasiswa','mahasiswa.nim = tiket.mahasiswa_id')
                     ->get();
         return $tiket->getResult();
     }
@@ -39,12 +39,21 @@ class ModelHelpdeskStaffDosen extends Model
     function getFAQ($topik_id){
         $faq = $this->db->table('faq')
                 ->where('topik_id', $topik_id)
-                ->join('topik', 'topik.id = faq.topik_id')
+                // ->join('topik', 'topik.id = faq.topik_id')
                 ->orderBy('created_at', 'desc')->get();
         return $faq->getResult();
     }
 
-    function insertFAQ($data){
+    function addFAQ($data){
         return $this->db->table('faq')->insert($data);
+    }
+
+    function deleteFAQ($faq_id){
+        
+        return $this->db->table('faq')->delete(['id'=> $faq_id]);
+    }
+
+    function updateFAQ($faq_id, $data){
+        return $this->db->table('faq')->update($data, ['id' => $faq_id]);
     }
 }
